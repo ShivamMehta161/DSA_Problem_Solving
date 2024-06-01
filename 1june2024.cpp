a96208286632f21c497d8971838849f2d07fe669
Shivam_Mehta <shivammehtadcm@gmail.com>
2024-06-01 23:29:15 +0530

    Question solved

---
+++ 1june2024.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+void sortColors(std::vector<int>& nums) {
+    int low = 0, mid = 0, high = nums.size() - 1;
+    
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            std::swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        } else if (nums[mid] == 1) {
+            mid++;
+        } else {  // nums[mid] == 2
+            std::swap(nums[high], nums[mid]);
+            high--;
+        }
+    }
+}
+
+int main() {
+    std::vector<int> nums1 = {2, 0, 2, 1, 1, 0};
+    sortColors(nums1);
+    for (int num : nums1) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl; // Output: 0 0 1 1 2 2
+
+    std::vector<int> nums2 = {2, 0, 1};
+    sortColors(nums2);
+    for (int num : nums2) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl; // Output: 0 1 2
+
+    return 0;
+}
