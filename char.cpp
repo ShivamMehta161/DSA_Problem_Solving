6ebd6dcf0377a7e735a23a100f44d9ec333439e8
Shivam_Mehta <shivammehtadcm@gmail.com>
2024-06-08 22:43:34 +0530

    char array strings

--- char.cpp
+++ char.cpp
@@ -3,6 +3,16 @@
 #include<string.h>
 #include<vector>
 using namespace std;
+void removeNonAlpha(string &s){
+        int i=0;
+        while(s[i]!='\0'){
+            if(!((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z') || (s[i]>='0' && s[i]<='9'))){
+                s.erase(i,1);
+            }
+            else{
+            i++;}
+        }
+    }
 int length(char a[], int size){
     int l=0;
     for(int i=0;i<size;i++){
@@ -45,8 +55,44 @@ void Lowerise(char a[],int size){
         }
     }
 }
-void ReplceWithSpace(char a[],int size){
-    
+void ReplaceWithSpace(char a[],int size){
+    for(int i=0;i<strlen(a);i++){
+        if(a[i]=='@'){
+            a[i]=' ';
+        }
+    }
+}
+bool checkPalindrome(char a[],int size){
+    bool ans=false;
+    int i=0;int j=size-1;
+    for (int k=0;k<size;k++){
+        if(a[i]==a[j]){
+            ans=true;
+            i++;j--;
+        }
+        else{
+            return false;
+        }
+    }
+    return ans;
+}
+string removeDuplicates(string s){
+    int i=0;
+    string ans="";
+    // ans[0]=s[0];
+    while(s[i]!='\0'){
+        if(ans.length()>0 && ans[ans.length()-1]==s[i]){
+            ans.pop_back();
+        }
+        else{
+            ans.push_back(s[i]);
+        }
+        
+        
+        
+        i++;
+            }
+    return ans;
 }
 int main(){
 // {   char a[100];
@@ -74,8 +120,16 @@ int main(){
     
     // cout<<d;
 //Replace @ with space
-char b[]="My@name@is@xyz.";
-int len=strlen(b);
-ReplaceWithSpace(b,len);
+// char b[]="My@name@is@xyz.";
+// int len=strlen(b);
+// // ReplaceWithSpace(b,len);
+// // cout<<endl;
+// // cout<<b;
+// cout<<checkPalindrome();
+// char a[]="bab";
+// cout<<checkPalindrome(a,3);
+string l="a abbc#%";
+removeNonAlpha(l);
+cout<<l;
     return 0;   
 }
